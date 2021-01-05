f72b8bb26bbdb4d58836f30aa9329a2402f322b8
sebnorth <sebnorth@gmail.com>
2021-01-05 11:53:51 +0100

    Dodaj pliki projektów.

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Program
+    {
+
+        struct Osoba {
+
+            public string imie;
+            public string nazwisko;
+            public string adresEmail;
+            public string nrTelefonu;
+        }
+
+        static void Main(string[] args)
+        {
+            Osoba os;
+
+            Console.WriteLine("Podaj imię: ");
+            os.imie = Console.ReadLine();
+
+            Console.WriteLine("Podaj nazwisko: ");
+            os.nazwisko = Console.ReadLine();
+
+            Console.WriteLine("Podaj adres e-mail: ");
+            os.adresEmail = Console.ReadLine();
+
+            Console.WriteLine("Podaj numer telefonu: ");
+            os.nrTelefonu = Console.ReadLine();
+        }
+    }
+}
